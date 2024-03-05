7d4e8d755d8b1543621b603cbf35c8f42eee12d4
anikdey095 <157403107+anikdey095@users.noreply.github.com>
2024-03-06 02:13:51 +0600

    Create quicksort.cpp

---
+++ quicksort.cpp
@@ -0,0 +1,66 @@
+#include<bits/stdc++.h>
+using namespace std;
+int partition(int arr[],int low,int high){
+    int i = low;
+    int j = high;
+    int pivot = arr[low];
+    while(i<j){
+        while(arr[i]<=pivot){
+            i++;
+        }
+        while(arr[j]>pivot){
+            j--;
+        }
+        if(i<j){
+            swap(arr[j], arr[i]);
+        }
+        else{
+            break;
+        }
+    }
+    swap(arr[j], arr[low]);
+    return j;
+}
+
+void quicksort(int arr[],int low,int high){
+    if(low<high){
+        int h = partition(arr, low, high);
+        quicksort(arr, low, h - 1);
+        quicksort(arr, h + 1, high);
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n;i++){
+        cin >> arr[i];
+    }
+    // for (int i = 0; i < n - 1;i++){
+    //     int min = i;
+    //     for (int j = i + 1; j < n;j++){
+    //         if(arr[j]<arr[min]){
+    //         min = j;
+    //     }
+    // }
+    // swap(arr[i], arr[min]);
+    // }
+
+    // for (int i = 1; i < n;i++){
+    //     int temp = arr[i];
+    //     int j = i - 1;
+    //     while (j >= 0 && arr[j] > temp){
+    //         arr[j + 1] = arr[j];
+    //         j--;
+    //     }
+    //     arr[j + 1] = temp;
+    // }
+
+    quicksort(arr, 0, n - 1);
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+        }
+}
